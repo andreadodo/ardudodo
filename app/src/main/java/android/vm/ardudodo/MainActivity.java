7682451b3b5c07f55fa8ba950f1c2ab0be406185
Alessio Cecchetti <Alessio Cecchetti>
2017-01-30 09:54:36 +0100

    main

--- app/src/main/java/android/vm/ardudodo/MainActivity.java
+++ app/src/main/java/android/vm/ardudodo/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 /**
  * Created by User on 26/01/2017.
@@ -17,6 +18,7 @@ public class MainActivity extends Activity {
     Button cameretta;
     Button bagno;
     Activity activity= this;
+    SeekBar seekbar;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
