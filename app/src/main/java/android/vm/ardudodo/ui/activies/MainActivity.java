491b6be8531a761bb1ca3696cee5b85e7a3879d8
Andrea <andrea.dodo@gmail.com>
2017-01-31 15:43:05 +0100

    Simplified main

--- app/src/main/java/android/vm/ardudodo/ui/activies/MainActivity.java
+++ app/src/main/java/android/vm/ardudodo/ui/activies/MainActivity.java
@@ -5,60 +5,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.vm.ardudodo.R;
+import android.vm.ardudodo.controllers.ValueActivity;
 import android.widget.Button;
 
 /**
  * Created by User on 26/01/2017.
+ * Modify by Andrea on 31/01/2017
  */
 
 public class MainActivity extends Activity {
-    Button cucina;
-    Button cameraLetto;
-    Button cameretta;
-    Button bagno;
-    Activity activity= this;
+    Button cucina, cameraLetto, cameretta, bagno;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cucina= (Button) findViewById(R.id.cucina);
+        //ID ASSOCIATION
+        cucina = (Button)findViewById(R.id.cucina);
+        cameraLetto = (Button)findViewById(R.id.cameraLetto);
+        cameretta = (Button)findViewById(R.id.cameretta);
+        bagno = (Button)findViewById(R.id.bagno);
+
+        //BUTTON ON CLICK LISTENER
         cucina.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(activity,KitchenActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(MainActivity.this,KitchenActivity.class));
             }
         });
-        cameraLetto= (Button) findViewById(R.id.cameraLetto);
         cameraLetto.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent2= new Intent(activity,BedroomActivity.class);
-                startActivity(intent2);
+                startActivity(new Intent(MainActivity.this,BedroomActivity.class));
             }
         });
-        cameretta= (Button) findViewById(R.id.cameretta);
         cameretta.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent3= new Intent(activity,KidRoomActivity.class);
-                startActivity(intent3);
+                startActivity(new Intent(MainActivity.this,KidRoomActivity.class));
             }
         });
-        bagno= (Button) findViewById(R.id.bagno);
         bagno.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 //TEST
-
-                startActivity(new Intent(activity,BathroomActivity.class));
-
-                //Intent intent4= new Intent(activity,BathroomActivity.class);
-                //Intent intent4= new Intent(activity,ValueActivity.class);
-                //startActivity(intent4);
+                //startActivity(new Intent(MainActivity.this,BathroomActivity.class));
+                startActivity(new Intent(MainActivity.this,ValueActivity.class));
             }
         });
-
     }
 }
