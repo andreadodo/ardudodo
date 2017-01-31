fc9d5803c93e803595e0efb0dcac4d7fc9e5a39b
Andrea <andrea.dodo@gmail.com>
2017-01-31 15:46:31 +0100

    Add Object Kitchen

--- app/src/main/java/android/vm/ardudodo/models/Kitchen.java
+++ app/src/main/java/android/vm/ardudodo/models/Kitchen.java
@@ -1,50 +1,76 @@
 package android.vm.ardudodo.models;
 
-import android.graphics.LightingColorFilter;
+import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import static android.vm.ardudodo.models.HouseIndex.*;
 
 /**
- * Created by andrea on 30/01/17.
+ * Created by andrea on 31/01/17.
  */
 
 public class Kitchen {
-    private boolean cucina,
-                    cucinaBox,
-                    corridoio;
+    private boolean cucina, cucinaBox, corridoio;
+    private int tapCucina, temperatura, umidita;
 
-    private int tapCucina,
-                temperatura,
-                umidita;
+    public Kitchen(JSONArray jsonValue) {
 
-    public Kitchen(boolean cucina, boolean cucinaBox, boolean corridoio, int tapCucina, int temperatura, int umidita) {
-        this.cucina = cucina;
-        this.cucinaBox = cucinaBox;
-        this.corridoio = corridoio;
-        this.tapCucina = tapCucina;
-        this.temperatura = temperatura;
-        this.umidita = umidita;
+        Log.d("VALUE JSON",jsonValue.toString());
+
+        Integer[] value = new Integer[jsonValue.length()];
+
+        for (int i = 0; i < jsonValue.length(); i++) {
+            try {
+                value[i] = jsonValue.getInt(i);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (int i = 0; i < value.length; i++)
+            Log.d("VALUE [" + i + "]", String.valueOf(value[i]));
+
+        this.cucina = (value[CUCINA] != 0);
+        this.cucinaBox = (value[CUCINA_BOX] != 0);
+        this.corridoio = (value[CORRIDOIO] != 0);
+        this.tapCucina = value[TAP_CUCINA];
+        this.temperatura = value[TEMPERATURA];
+        this.umidita = value[UMIDITA];
     }
 
-    public boolean isCucina() {
+    public boolean getCucina() {
         return cucina;
     }
+    public void setCucina(boolean cucina) {
+        this.cucina = cucina;
+    }
 
-    public boolean isCucinaBox() {
+    public boolean getCucinaBox() {
         return cucinaBox;
+    }
+    public void setCucinaBox(boolean cucinaBox) {
+        this.cucinaBox = cucinaBox;
+    }
+
+    public boolean getCorridoio() {
+        return corridoio;
+    }
+    public void setCorridoio(boolean corridoio) {
+        this.corridoio = corridoio;
     }
 
     public int getTapCucina() {
         return tapCucina;
     }
-
-    public boolean isCorridoio() {
-        return corridoio;
+    public void setTapCucina(int tapCucina) {
+        this.tapCucina = tapCucina;
     }
 
-    public int getTemperatura() {
-        return temperatura;
+    public void setTemperatura(int temperatura) {
+        this.temperatura = temperatura;
     }
-
-    public int getUmidita() {
-        return umidita;
+    public void setUmidita(int umidita) {
+        this.umidita = umidita;
     }
 }
