4f2bf860cb94706625e66694618dd792bd54cc4c
Andrea <andrea.dodo@gmail.com>
2017-01-31 18:08:05 +0100

    Add temp + humd on kitchen + minor

--- app/src/main/java/android/vm/ardudodo/ui/activies/KitchenActivity.java
+++ app/src/main/java/android/vm/ardudodo/ui/activies/KitchenActivity.java
@@ -7,30 +7,40 @@ import android.vm.ardudodo.R;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 
 /**
  * Created by User on 26/01/2017.
  */
 
 public class KitchenActivity extends Activity {
-    Switch switchCucina;
+    Switch switchCucina, switchCorridoio;
     SeekBar seekBarCucina;
-
+    TextView temp, umid;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kitchen);
 
         switchCucina = (Switch) findViewById(R.id.switch_cucina);
+        switchCorridoio = (Switch) findViewById(R.id.switch_corridoio);
         seekBarCucina = (SeekBar) findViewById(R.id.seekBar_cucina);
-        seekBarCucina.incrementProgressBy(10);
-
+        temp = (TextView) findViewById(R.id.tv_temperatura);
+        umid = (TextView) findViewById(R.id.tv_umidita);
 
         switchCucina.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton s, boolean isChecked) {
                 Log.d("SW CUCINA", String.valueOf(isChecked));
-                //TBD SEND TO PHP
+                // TODO SEND TO PHP
+            }
+        });
+
+        switchCorridoio.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton s, boolean isChecked) {
+                Log.d("SW CORRIDOIO", String.valueOf(isChecked));
+                // TODO SEND TO PHP
             }
         });
 
@@ -38,7 +48,7 @@ public class KitchenActivity extends Activity {
             int val;
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                progress = progress / 10;
+                progress = progress / 10; //TODO IMPROVE USER EXPERIENCE
                 progress = progress * 10;
                 val=progress;
                // seekBar.incrementProgressBy(1);
@@ -50,8 +60,12 @@ public class KitchenActivity extends Activity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 seekBarCucina.setProgress(val);
-                Log.d("SK CUCINA", String.valueOf(val));
+                Log.d("SK CUCINA", String.valueOf(val)); //TODO SEND TO PHP
             }
         });
+
+        temp.setText("10°C"); //TODO READ
+        umid.setText("30%");
+
     }
 }
