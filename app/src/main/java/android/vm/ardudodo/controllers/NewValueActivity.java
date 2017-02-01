be77eedd8066f0e7993e044e46ccc6d82e35628b
Andrea <andrea.dodo@gmail.com>
2017-02-01 09:20:03 +0100

    Jsonarray

--- app/src/main/java/android/vm/ardudodo/controllers/NewValueActivity.java
+++ app/src/main/java/android/vm/ardudodo/controllers/NewValueActivity.java
@@ -8,36 +8,37 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import static android.vm.ardudodo.models.HouseIndex.URL;
+
 
 /**
- * Created by andrea on 30/01/17.
+ * Created by andrea on 31/01/17.
  */
 
-public class ValueActivity extends Activity{
+public class NewValueActivity extends Activity{
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            fetchDataFromUdoo();
-            //  Log.d("")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fetchDataFromUdoo();
+        //  Log.d("")
 
-        }
+    }
 
     private void fetchDataFromUdoo() {
 
         //Init request queque
         RequestQueue queue = Volley.newRequestQueue(this);
-        //String url = "http://www.mocky.io/v2/588b5b94300000411cfa8dd0";
-        String url = "http://172.16.9.150:8888/ardudodo.php";
 
         //Request a string response from the URL
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
                 new Response.Listener<String>(){
                     @Override
                     public void onResponse(String response){
@@ -50,19 +51,6 @@ public class ValueActivity extends Activity{
                             e.printStackTrace();
                         }
                         Log.d("JSON",jsonArray.toString());
-
-                        Integer[] value = new Integer[jsonArray.length()];
-
-                        for (int i = 0; i < jsonArray.length(); i++) {
-                            try {
-                                value[i] = jsonArray.getInt(i);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                        for (int i = 0; i < value.length; i++)
-                            Log.d("VALUE [" + i + "]", String.valueOf(value[i]));
                     }
                 },
 
